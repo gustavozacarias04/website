84cd7ceedd45465e18d0a9fb746a97f692ef1ca1
gustavo <gustavozacarias2004@gmail.com>
2024-10-19 18:42:08 +0100

    Tailwind home page correct

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Skills from './components/Skills';
@@ -13,6 +13,8 @@ function App() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/website" element={<Home />} />
+                <Route path="*" element={<Navigate to="/" />} />
                 <Route path="/skills" element={<Skills />} />
                 <Route path="/projects" element={<Projects />} />
                 <Route path="/about" element={<About />} />
